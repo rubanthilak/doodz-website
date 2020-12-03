e8ac8cc338f68fcc95f65bd0ad85d3677610ef82
Ruban Thilak <rubanthilak.24@gmail.com>
2020-12-03 13:23:37 +0530

    v0.1

---
+++ src/js/main.js
@@ -0,0 +1,98 @@
+const sal = require('sal.js');
+const $ = require('jquery');
+
+sal({
+  threshold: .5,
+  once: false,
+});
+
+if(!hasTouch())
+{
+  $(document).ready(function(){
+    $('#wrapper').scroll(function(){
+      var light_pos = $('#home').offset().top;
+      var light_height = $('#home').height();
+      var menu_pos = $('.menu-bar').offset().top;  
+  
+      if(menu_pos < (light_pos + light_height)) {
+          $('.bar1').addClass('bar-black');
+          $('.bar1').removeClass('bar-white');
+          $('.bar2').addClass('bar-black');
+          $('.bar2').removeClass('bar-white');
+      }
+      else {
+          $('.bar1').removeClass('bar-black');
+          $('.bar1').addClass('bar-white');
+          $('.bar2').removeClass('bar-black');
+          $('.bar2').addClass('bar-white');
+      }  
+    })
+  })
+}
+
+function toggleMenu() {
+  document.getElementById("menu-icon").classList.toggle("change");
+  var id = document.getElementById("sidenav");
+  var oid = document.getElementById("op-layer");
+  if(id.style.width == "75%")
+  {
+    id.style.width = "0vh";
+    oid.style.display = "none";
+    if(findBackgroundColor() && !hasTouch())
+    {
+      $('.bar1').removeClass('bar-black');
+      $('.bar1').addClass('bar-white');
+      $('.bar2').removeClass('bar-black');
+      $('.bar2').addClass('bar-white');
+    }
+  }
+  else
+  {
+    id.style.width = "75%";
+    oid.style.display = "block";
+    $('.bar1').addClass('bar-black');
+    $('.bar1').removeClass('bar-white');
+    $('.bar2').addClass('bar-black');
+    $('.bar2').removeClass('bar-white');
+  }
+}
+
+// inorder to bundle the js file with other library, 
+// onclick function might not be include when bundling,
+// so we need to include this code below
+window.toggleMenu = toggleMenu; 
+
+function findBackgroundColor() {
+    var light_pos = $('#home').offset().top;
+    var light_height = $('#home').height();
+    var menu_pos = $('.menu-bar').offset().top;  
+    if(menu_pos < (light_pos + light_height)) {
+        return false;
+    }
+    else {
+        return true;
+    }  
+}
+
+function hasTouch() {
+  return 'ontouchstart' in document.documentElement
+         || navigator.maxTouchPoints > 0
+         || navigator.msMaxTouchPoints > 0;
+}
+
+if (hasTouch()) { // remove all the :hover stylesheets when using mobile
+  try { // prevent exception on browsers not supporting DOM styleSheets properly
+    for (var si in document.styleSheets) {
+      var styleSheet = document.styleSheets[si];
+      if (!styleSheet.rules) continue;
+
+      for (var ri = styleSheet.rules.length - 1; ri >= 0; ri--) {
+        if (!styleSheet.rules[ri].selectorText) continue;
+
+        if (styleSheet.rules[ri].selectorText.match(':hover')) {
+          styleSheet.deleteRule(ri);
+        }
+      }
+    }
+  } catch (ex) {}
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,88 @@
 var path = require('path');
 var webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = {
-    entry: path.resolve(__dirname, 'src/script/main.js'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: "bundle.js",
-        publicPath: '/dist'    
+    entry: {
+        app: './src'
     },
+    output: {
+        path: path.resolve(__dirname, 'static'),
+        filename: "main.js",
+        publicPath: '/static'    
+    },
+    mode: 'production',
     module: {
         rules: [
+            {
+                test: /\.(ttf)$/i,
+                use: [
+                  {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: '../fonts',
+                        publicPath: '../fonts', // this is added inorder to overwrite the url() in the css file.
+                    },
+                  },
+                ],
+            },
+            {
+                test: /\.(svg)$/i,
+                use: [
+                  {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]',
+                        outputPath: '../assets',
+                        publicPath: '../assets', // this is added inorder to overwrite the url() in the css file.
+                    },
+                  },
+                ],
+            },
             {
                 test: /\.css$/,
                 use: [
                     'style-loader',
                     'css-loader'
                 ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    // 'style-loader',
+                    {
+                        loader: 'css-loader', //translates CSS into CommonJS
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'resolve-url-loader',
+                        options: {
+                            sourceMap: true,
+                        }
+                    },
+                    {
+                        loader: 'sass-loader', //compiles Sass to CSS, using Node-Sass by default
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
             }
         ],
     },
+    resolve: {
+        extensions: ['.ts', '.scss', '.css', '.js'],
+        modules: [
+            path.resolve(__dirname, "node_modules"),
+        ],
+    },
     plugins: [
-        new webpack.ProvidePlugin({
-            $: 'jquery'
-        })
-    ]
+        new MiniCssExtractPlugin({
+            filename: 'style.css',
+        }),
+    ],
 }
